d77c3d4ffd64f3948ef0d6995aef32a37a5b339a
nizam323 <muhammadnizamuddin.sce@gmail.com>
2024-05-06 19:34:29 +0500

    FAQ section design completed

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // FAQ btn logic
 
 function clk(elements) {
+    let qe = elements.lastElementChild;
     let answ = elements.nextElementSibling;
-
+    
     let unHide = document.querySelector(".d-block");
-
-    console.log(answ)
+    
+    let reslt = answ.classList.contains("d-block");
+    
+    if (reslt == false) {
+        answ.classList.toggle("d-block");
+        qe.classList.add("btn-animation")
+    }
+    
+    unHide.classList.remove("d-block")
+    qe.classList.remove("btn-animation")
+    
+    
 };
